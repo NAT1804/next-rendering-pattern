3fcfe031c54a6cbff6acae8fee1bf98fef6b79dc
Nguyễn Anh Tuấn <michigo2802@gmail.com>
2023-02-20 01:16:04 +0700

    Update isr isr-on-demand

--- src/app/isr-on-demand/page.tsx
+++ src/app/isr-on-demand/page.tsx
@@ -1,27 +1,21 @@
-// Incremental static generation on demand
+// server side rendered
+import { API } from "@/config/api";
+import { use } from "react";
 
-import { API } from '@/config/api';
+async function getData() {
+  return await (await fetch(API, { cache: "no-store" })).json();
+}
 
-export default function ISROD({ data }: any) {
+export default function SSR() {
+
+  const users = use<any>(getData());
+
   return (
     <>
-      {data.map((e: any) => (
+      {users.map((e: any) => (
         <h2 key={e.id}>{e.name}</h2>
       ))}
     </>
   );
 }
 
-// This function gets called at build time on server-side.
-// It may be called again, on a serverless function, if
-// the api endpoint e.g. api/revalidate get's pinged.
-export async function getStaticProps() {
-  const res = await fetch(API);
-  const data = await res.json();
-
-  return {
-    props: {
-      data, // will be passed to the page component as props
-    },
-  };
-}
--- src/app/isr/page.tsx
+++ src/app/isr/page.tsx
@@ -1,32 +1,21 @@
-// Incremental static generation
+// server side rendered
+import { API } from "@/config/api";
+import { use } from "react";
 
-import { API } from '@/config/api';
+async function getData() {
+  return await (await fetch(API, { cache: "no-store" })).json();
+}
 
-export default function IncrementalStaticRegeneration({ data }: any) {
+export default function SSR() {
+
+  const users = use<any>(getData());
+
   return (
     <>
-      {data.map((e: any) => (
+      {users.map((e: any) => (
         <h2 key={e.id}>{e.name}</h2>
       ))}
     </>
   );
 }
 
-// This function gets called at build time on server-side.
-// It may be called again, on a serverless function, if
-// revalidation is enabled and a new request comes in
-export async function getStaticProps() {
-  const res = await fetch(API);
-  const data = await res.json();
-
-  return {
-    props: {
-      data, // will be passed to the page component as props
-    },
-
-    // Next.js will attempt to re-generate the page:
-    // - When a request comes in
-    // - At most once every second
-    revalidate: 100, // In seconds
-  };
-}
